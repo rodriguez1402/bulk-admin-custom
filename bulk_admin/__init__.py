11df4df384a1a3aac333682e55669043d0a8fd9f
rodriguez1402 <oscarduvan.rodriguez@technoapes.co>
2024-04-12 11:02:48 -0500

    first commit

---
+++ bulk_admin/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from bulk_admin.admin import (
+    BulkModelAdmin,
+    StackedBulkInlineModelAdmin,
+    TabularBulkInlineModelAdmin,
+)
+
+__all__ = [
+    BulkModelAdmin,
+    StackedBulkInlineModelAdmin,
+    TabularBulkInlineModelAdmin,
+]
